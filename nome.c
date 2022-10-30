ab8747e596c41eb4b3da0e68e06ad21421783f2d
João Afonso Vitorino Reis <103146935+Rexxisssss@users.noreply.github.com>
2022-10-30 14:11:58 +0000

    Add files via upload

---
+++ nome.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    
+    char nome[999];
+    printf ("Introduz o teu nome: ");
+    scanf ("%s", &nome);
+    
+    printf ("O teu nome é: %s ", nome);
+    return 0;
+            
+    }
