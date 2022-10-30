f5a48ccc519805423643d01052490d84a0fe3a1a
João Afonso Vitorino Reis <103146935+Rexxisssss@users.noreply.github.com>
2022-10-30 13:49:54 +0000

    Add files via upload

---
+++ numero inteiro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    
+    int numero;
+    
+        printf ("Digite o número inteiro: ");
+        scanf ("%d", &numero);
+        
+        printf ("O número inteiro introduzido foi: %d ", numero);
+        return 0;
+        
+}
