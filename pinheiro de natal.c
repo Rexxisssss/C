57b7f92119308211b6499389a190e1d43e923763
João Afonso Vitorino Reis <103146935+Rexxisssss@users.noreply.github.com>
2022-12-12 15:20:09 +0000

    Add files via upload

---
+++ pinheiro de natal.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+    
+    printf("%16s", "@\n");
+    printf("%17s", "XXX\n");
+    printf("%18s", "XXXXX\n");
+    printf("%19s", "XXX§XX\n");
+    printf("%20s", "XXoXXXXXX\n");
+    printf("%21s", "XXX$XXXXXXX\n");
+    printf("%22s", "X$XXXXX&XXXX\n");
+    printf("%23s", "XXXXXXXXXXXXXXX\n");
+    printf("%17s", "XX\n");
+    printf("%17s", "XX\n");
+    printf("%18s", "XXXX\n");
+}
